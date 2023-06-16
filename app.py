20c1ec19132a5eb91ec8e30ece3d0bebaa4c3a82
Dr. Meghdoot Ghosh <122888458+meghdoot1234@users.noreply.github.com>
2023-06-15 23:35:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,21 @@ st.markdown("Toy model to play to classify iris flowers into setosa, versicolor,
 st.header("Plant Features")
 col1, col2 = st.columns(2)
 with col1:
+    a = float(1.0)
+    b = float(8.0)
+    c = float(0.5)
+    d = float(2.0)
+    e = float(4.4)
+    f = float(7.0)
+    g = float(0.1)
+    h = float(2.5)
     st.text("Sepal characteristics")
-    sepal_l = st.slider("Sepal lenght (cm)", 1.0, 8.0, 0.5)
-    sepal_w = st.slider("Sepal width (cm)", 2.0, 4.4, 0.5)
+    sepal_l = st.slider("Sepal lenght (cm)", a, b, c)
+    sepal_w = st.slider("Sepal width (cm)", d, e, c)
 with col2:
     st.text("Pepal characteristics")
-    petal_l = st.slider("Petal lenght (cm)", 1.0, 7.0, 0.5)
-    petal_w = st.slider("Petal width (cm)", 0.1, 2.5, 0.5)
+    petal_l = st.slider("Petal lenght (cm)", a, f, c)
+    petal_w = st.slider("Petal width (cm)", g, h, c)
 
 st.text('')
 if st.button("Predict type of Iris"):
